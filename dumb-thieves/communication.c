8a6494f2f45d4bad11d6ff7a605ac87c9df2f119
hkereb <115366640+hkereb@users.noreply.github.com>
2025-04-26 19:20:12 +0200

    fix lang and increase hosts slots

--- dumb-thieves/communication.c
+++ dumb-thieves/communication.c
@@ -6,7 +6,7 @@
 void send_message(int dest, int rank) {
     increment_clock();
     MPI_Send(&lamport_clock, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-    printf("[Proces %d] Wysłał wiadomość do %d (czas lokalny = %d)\n", rank, dest, lamport_clock);
+    printf("[Process %d] Sent message to %d (local time = %d)\n", rank, dest, lamport_clock);
 }
 
 void receive_message(int rank) {
@@ -15,5 +15,5 @@ void receive_message(int rank) {
 
     MPI_Recv(&received_time, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
     update_clock(received_time);
-    printf("[Proces %d] Otrzymał wiadomość od %d (czas lokalny = %d)\n", rank, status.MPI_SOURCE, lamport_clock);
+    printf("[Process %d] Received message from %d (local time = %d)\n", rank, status.MPI_SOURCE, lamport_clock);
 }
